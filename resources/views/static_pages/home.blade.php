9c371352fdba169d711843af90c5250e9867e713
dustreading <dustreading@163.com>
2019-10-11 02:38:29 +0800

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,6 +4,6 @@
         <h1>Hello Laravel</h1>
         <p class="lead">你现在所看到的是 <a href="https://laravel-china.org">Laravel中国</a>的示例项目主页</p>
         <p>一切，将从这里开始。</p>
-        <p><a class="btn btn-lg btn-success" href="#" role="botton">现在注册</a></p>
+        <p><a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="botton">现在注册</a></p>
     </div>
 @stop
