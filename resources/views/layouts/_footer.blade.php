9d142d782bb33346668c88958ed615af5620c8a0
dustreading <dustreading@163.com>
2019-10-10 18:03:00 +0800

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,6 +2,6 @@
     <img class="brand-icon" src="https://www.easyicon.net/download/png/1225464/512/"/>
     <a href="https://laravel-china.org/courses" target=_blank>刻意练习，每日精选</a>
     <div class="float-right">
-        <a href="/about">关于</a>
+    <a href="{{ route('help') }}">关于</a>
     </div>
 </footer>
